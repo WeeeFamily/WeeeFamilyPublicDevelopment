c3932ed6b6614f1b3ce8f50817b5bbd4715e63e1
WeeeFamily <maaxsvt@gmail.com>
2024-07-18 00:23:56 -0800

    5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+import {useTelegram} from "./Components/hooks/useTelegram";
 import header from "./Components/header/header";
 
 function App() {
