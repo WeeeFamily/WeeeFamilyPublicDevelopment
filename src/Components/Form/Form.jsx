0fa2c58c0b89adbc6c546af5475ee3c425227484
WeeeFamily <maaxsvt@gmail.com>
2024-07-18 03:58:30 -0800

    5

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 import './Form.css';
 const Form = () => {
     return (
-        <div>
-            Form
+        <div className={"Form"}>
+            <h3>Для бронирования Автомобиля, введите данные ниже:</h3>
+            <select className={'select'}>
+                <option value={'delivery'}>Батуми</option>
+                <option value={'base'}>Тбилиси</option>
+            </select>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Ваше Имя'}/>
+            <input
+                className={'input'}
+                type='text' placeholder={'Номер телефона'}/>
+            <input
+                className={'input'}
+                type='text'
+                placeholder={'Дата начала аренды'}/>
+
+            <select className={'select'}>
+                <option value={'delivery'}>Доставка автомобиля </option>
+                <option value={'base'}> Забрать на офисе </option>
+            </select>
+
+
+
         </div>
     );
 };
