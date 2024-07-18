5a2c131f59bd7c12e89645ac9dfe9c430a277cc1
WeeeFamily <maaxsvt@gmail.com>
2024-07-17 23:06:32 -0800

    455

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./Components/hooks/useTelegram"; 
-import header from "./Components/Header/Header";
+import header from "../../Components/Header/Header";
 
 function App() {
     const {onToggleButton,tg} =useTelegram();
