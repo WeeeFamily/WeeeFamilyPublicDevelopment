5266771bafd5ee270efbe2294cb379a5486a41e2
WeeeFamily <maaxsvt@gmail.com>
2024-07-19 03:08:21 -0800

    5

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -12,7 +12,7 @@ const ProductItem = ({product, className,onAdd}) => {
         <div className={'product' + className}>
             <div className={'img'}/>
             <div className={'title'}>{product.title}/</div>
-            <div className={'description'}>{product.title}/</div>>
+            <div className={'description'}>{product.description}/</div>>
             <div className={'price'}>
              <span>Стоимость: <b>{product.price}</b> </span>
             </div>
