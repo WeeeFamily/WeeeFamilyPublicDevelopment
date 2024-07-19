677592d1f197b705e843cc77349a03aa513088c6
WeeeFamily <maaxsvt@gmail.com>
2024-07-19 03:38:44 -0800

    5

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from "../button/Button";
-import './ProductItem.css';
+// import './ProductItem.css';
 
 
 const ProductItem = ({product, className,onAdd}) => {
