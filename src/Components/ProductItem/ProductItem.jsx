a2a41bbc53bdfb3a7e9dbd0cbbde11669883484c
WeeeFamily <maaxsvt@gmail.com>
2024-07-21 08:47:57 -0800

    5

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -13,9 +13,9 @@ const ProductItem = ({product, className,onAdd}) => {
 
     return (
         <div className={'product' + className}>
-            <div className={'img'}/>
-            <div className={'title'}>{product.title}/</div>
-            <div className={'description'}>{product.description}/</div>
+            <div className={'img'}>t{product.img}/</div>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
             <div className={'price'}>
              <span>Стоимость: <b>{product.price}</b> </span>
             </div>
--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -6,58 +6,58 @@ import {useTelegram} from "../hooks/useTelegram";
 
 
 const products = [
-    {id: '1', title: 'Tayota Rav 4', price: 100, description: 'Идеально подходит для города, путешествий и горной местности'},
-    {id: '2', title: 'Volkswagen ЖУК', price: 50, description: 'Идеально подходит для съемок и мероприятий'},
-    {id: '3', title: 'Dodje Chardger', price: 110, description: 'Идеально подъодит для города и съемок, езды за городом'},
+    {id: '1', title: 'Tayota Rav 4', price: 100, description: 'Идеально подходит для города, путешествий и горной местности', img:'/PhotoCars/toyota-rav4.jpg'},
+    {id: '2', title: 'Volkswagen ЖУК', price: 50, description: 'Идеально подходит для съемок и мероприятий',img:'/PhotoCars/volkswagen-zhuk.jpg'},
+    {id: '3', title: 'Dodje Chardger', price: 110, description: 'Идеально подъодит для города и съемок, езды за городом',img:'/PhotoCars/dodge-charger.jpg'},
 ]
 
-const getTotalPrice = (item = []) => {
-    return item.reduce((acc, item) => {
-        return acc+=item.price
-        },0)
-}
+// const getTotalPrice = (item = []) => {
+//     return item.reduce((acc, item) => {
+//         return acc+=item.price
+//         },0)
+// }
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
     const {tg} = useTelegram();
-    const onAdd = (product) => {
+    // const onAdd = (product) => {
+    //
+    //     const alredyAdded = addedItems.find(item => item.id === product.id);
+    //     let newItems= [];
+    //
+    //     if(alredyAdded) {
+    //         newItems = addedItems.filter(item => item.id !== product.id);
+    //     }
+    //     else {
+    //         newItems = [...addedItems, product];
+    //     }
+    //
+    //     setAddedItems(newItems)
+    //
+    //     if (newItems.length === 0) {
+    //         tg.MainButton.hide();
+    //     }
+    //     else {
+    //         tg.MainButton.show();
+    //
+    //         tg.MainButton.setParams({
+    //             text:`Rent for ${getTotalPrice(newItems)}$`
+    //         })
+    //     }
+    // }
 
-        const alredyAdded = addedItems.find(item => item.id === product.id);
-        let newItems= [];
 
-        if(alredyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
-        }
-        else {
-            newItems = [...addedItems, product];
-        }
-
-        setAddedItems(newItems)
-
-        if (newItems.length === 0) {
-            tg.MainButton.hide();
-        }
-        else {
-            tg.MainButton.show();
-
-            tg.MainButton.setParams({
-                text:`Rent for ${getTotalPrice(newItems)}$`
-            })
-        }
-    }
-
-
-    return (
-        <div className={'list'}>
-            {products.map(item => (
-           <ProductItem
-            product={item}
-            onAdd={onAdd}
-            className={'item'}
-                />
-             ))}
-
-        </div>
-    );
+    // return (
+    //     <div className={'list'}>
+    //         {products.map(item => (
+    //        <ProductItem
+    //         product={item}
+    //         onAdd={onAdd}
+    //         className={'item'}
+    //             />
+    //          ))}
+    //
+    //     </div>
+    // );
 };
 
 
