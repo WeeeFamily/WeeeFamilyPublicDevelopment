bb2d508c4eda75bc620fdb1c5291cddac809a832
WeeeFamily <maaxsvt@gmail.com>
2024-07-22 03:42:55 -0800

    5

---
+++ src/Components/Filters/FilterBar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './FilterBar.css';
+
+const FilterBar = ({ onFilterChange }) => {
+    return (
+        <div className="filter-container">
+            <div className="filter">
+                <button onClick={() => onFilterChange('all')} className="filter-button">
+                    <i className="fas fa-car"></i>
+                    <span>Все автомобили</span>
+                </button>
+            </div>
+            <div className="filter">
+                <i className="fas fa-handshake"></i>
+                <select onChange={(e) => onFilterChange('partner', e.target.value)}>
+                    <option value="">Партнеры</option>
+                    <option value="Partner 1">Partner 1</option>
+                    <option value="Partner 2">Partner 2</option>
+                </select>
+
+            </div>
+            <div className="filter">
+                <i className="fas fa-dollar-sign"></i>
+                <select onChange={(e) => onFilterChange('price', e.target.value)}>
+                    <option value="">По цене</option>
+                    <option value="price_asc">От низкой к высокой</option>
+                    <option value="price_desc">От высокой к низкой</option>
+                </select>
+
+            </div>
+            <div className="filter">
+            <i className="fas fa-car-side"></i>
+                <select onChange={(e) => onFilterChange('class', e.target.value)}>
+                    <option value="">Класс автомобиля</option>
+                    <option value="SUV">SUV</option>
+                    <option value="Compact">Compact</option>
+                    <option value="Sedan">Sedan</option>
+                </select>
+
+            </div>
+            <div className="filter">
+                <i className="fas fa-industry"></i>
+                <select onChange={(e) => onFilterChange('brand', e.target.value)}>
+                    <option value="">Марка авто</option>
+                    <option value="Toyota">Toyota</option>
+                    <option value="Volkswagen">Volkswagen</option>
+                    <option value="Dodge">Dodge</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default FilterBar;
--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,64 +1,82 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './ProductList.css';
-import ProductItem  from "../ProductItem/ProductItem";
-import {useTelegram} from "../hooks/useTelegram";
-
-
+import ProductItem from "../ProductItem/ProductItem";
+import { useTelegram } from "../hooks/useTelegram";
+import FilterBar from "../Filters/FilterBar";
 
 const products = [
-    {id: '1', title: 'Tayota Rav 4', price: 100, description: 'Идеально подходит для города, путешествий и горной местности', img:'/PhotoCars/toyota-rav4.jpg'},
-    {id: '2', title: 'Volkswagen ЖУК', price: 50, description: 'Идеально подходит для съемок и мероприятий',img:'/PhotoCars/volkswagen-zhuk.jpg'},
-    {id: '3', title: 'Dodje Chardger', price: 110, description: 'Идеально подъодит для города и съемок, езды за городом',img:'/PhotoCars/dodge-charger.jpg'},
-]
+    { id: '1', title: 'Toyota Rav 4', price: 100, description: 'Идеально подходит для города, путешествий и горной местности', img: '/PhotoCars/toyota-rav4.jpg', brand: 'Toyota', class: 'SUV', partner: 'Partner 2' },
+    { id: '2', title: 'Volkswagen Жук', price: 50, description: 'Идеально подходит для съемок и мероприятий', img: '/PhotoCars/volkswagen-zhuk.jpg', brand: 'Volkswagen', class: 'Compact', partner: 'Partner 1' },
+    { id: '3', title: 'Dodge Charger', price: 110, description: 'Идеально подходит для города и съемок, езды за городом', img: '/PhotoCars/dodge-charger.jpg', brand: 'Dodge', class: 'Sedan', partner: 'Partner 1' },
+];
 
-const getTotalPrice = (item = []) => {
-    return item.reduce((acc, item) => {
-        return acc+=item.price
-        },0)
-}
+
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg} = useTelegram();
-    const onAdd = (product) => {
+    const [filteredProducts, setFilteredProducts] = useState(products);
+    const { tg } = useTelegram();
 
-        const alredyAdded = addedItems.find(item => item.id === product.id);
-        let newItems= [];
 
-        if(alredyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
-        }
-        else {
-            newItems = [...addedItems, product];
-        }
-
-        setAddedItems(newItems)
-
-        if (newItems.length === 0) {
-            tg.MainButton.hide();
-        }
-        else {
-            tg.MainButton.show();
-
-            tg.MainButton.setParams({
-                text:`Rent for ${getTotalPrice(newItems)}$`
-            })
-        }
+     const sortProducts = (order, type) => {
+        const sorted = [...filteredProducts].sort((a, b) => {
+            if (type === 'price') {
+                if (order === 'asc') {
+                    return a.price - b.price;
+                } else if (order === 'desc') {
+                    return b.price - a.price;
+                }
+            } else if (type === 'brand') {
+                if (order === 'asc') {
+                    return a.brand.localeCompare(b.brand);
+                } else if (order === 'desc') {
+                    return b.brand.localeCompare(a.brand);
+                }
+            } else if (type === 'class') {
+                if (order === 'asc') {
+                    return a.class.localeCompare(b.class);
+                } else if (order === 'desc') {
+                    return b.class.localeCompare(a.class);
+                }
+            }
+            return 0;
+        });
+        setFilteredProducts(sorted);
     }
 
 
-    return (
-        <div className={'list'}>
-            {products.map(item => (
-           <ProductItem
-            product={item}
-            onAdd={onAdd}
-            className={'item'}
-                />
-             ))}
+    const handleFilterChange = (type, value) => {
+        if (type === 'all') {
+            setFilteredProducts(products);
+        } else if (type === 'partner') {
+            const filtered = products.filter(product => product.partner === value);
+            setFilteredProducts(filtered);
+        } else if (type === 'price') {
+            const [order] = value.split('_');
+            sortProducts(order, 'price');
+        } else if (type === 'class') {
+            const filtered = products.filter(product => product.class === value);
+            setFilteredProducts(filtered);
+        } else if (type === 'brand') {
+            const filtered = products.filter(product => product.brand === value);
+            setFilteredProducts(filtered);
+        }
+    }
+return (
+        <div className={'product-list'}>
+            <FilterBar onFilterChange={handleFilterChange} />
+            <div className={'list'}>
+                {filteredProducts.map(item => (
+                    <ProductItem
+                        key={item.id}
+                        product={item}
 
+                        className={'item'}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
 
-
 export default ProductList;
+
