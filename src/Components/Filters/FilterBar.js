0f93b714265a300fb5c73d92ebe2661ba8f4efee
WeeeFamily <maaxsvt@gmail.com>
2024-07-22 23:04:11 -0800

    01234

--- src/Components/Filters/FilterBar.js
+++ src/Components/Filters/FilterBar.js
@@ -7,31 +7,31 @@ const FilterBar = ({ onFilterChange }) => {
             <div className="filter">
                 <button onClick={() => onFilterChange('all')} className="filter-button">
                     <i className="fas fa-car"></i>
-                    <span>Все автомобили</span>
+                    <span>All cars</span>
                 </button>
             </div>
             <div className="filter">
                 <i className="fas fa-handshake"></i>
                 <select onChange={(e) => onFilterChange('partner', e.target.value)}>
-                    <option value="">Партнеры</option>
-                    <option value="Partner 1">Partner 1</option>
-                    <option value="Partner 2">Partner 2</option>
+                    <option value="">Partners</option>
+                    <option value="Partner 1">Jet Car</option>
+                    <option value="Partner 2">Weee Cars</option>
                 </select>
 
             </div>
             <div className="filter">
                 <i className="fas fa-dollar-sign"></i>
                 <select onChange={(e) => onFilterChange('price', e.target.value)}>
-                    <option value="">По цене</option>
-                    <option value="asc">От низкой к высокой</option>
-                    <option value="desc">От высокой к низкой</option>
+                    <option value="">For price:</option>
+                    <option value="asc">Low-Hight</option>
+                    <option value="desc">Hight-Low</option>
                 </select>
 
             </div>
             <div className="filter">
             <i className="fas fa-car-side"></i>
                 <select onChange={(e) => onFilterChange('class', e.target.value)}>
-                    <option value="">Класс автомобиля</option>
+                    <option value="">Class car</option>
                     <option value="SUV">SUV</option>
                     <option value="Compact">Compact</option>
                     <option value="Sedan">Sedan</option>
@@ -41,7 +41,7 @@ const FilterBar = ({ onFilterChange }) => {
             <div className="filter">
                 <i className="fas fa-industry"></i>
                 <select onChange={(e) => onFilterChange('brand', e.target.value)}>
-                    <option value="">Марка авто</option>
+                    <option value="">Model car</option>
                     <option value="Toyota">Toyota</option>
                     <option value="Volkswagen">Volkswagen</option>
                     <option value="Dodge">Dodge</option>
--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -5,9 +5,9 @@ import { useTelegram } from "../hooks/useTelegram";
 import FilterBar from "../Filters/FilterBar";
 
 const products = [
-    { id: '1', title: 'Toyota Rav 4', price: 100, description: 'Идеально подходит для города, путешествий и горной местности', img: '/PhotoCars/toyota-rav4.jpg', brand: 'Toyota', class: 'SUV', partner: 'Partner 2' },
-    { id: '2', title: 'Volkswagen Жук', price: 50, description: 'Идеально подходит для съемок и мероприятий', img: '/PhotoCars/volkswagen-zhuk.jpg', brand: 'Volkswagen', class: 'Compact', partner: 'Partner 1' },
-    { id: '3', title: 'Dodge Charger', price: 110, description: 'Идеально подходит для города и съемок, езды за городом', img: '/PhotoCars/dodge-charger.jpg', brand: 'Dodge', class: 'Sedan', partner: 'Partner 1' },
+    { id: '1', title: 'Toyota Rav 4', price: 100, description: 'Ideal for city, travel and mountainous areas.', img: '/PhotoCars/toyota-rav4.jpg', brand: 'Toyota', class: 'SUV', partner: 'Partner 2' },
+    { id: '2', title: 'Volkswagen Zhuk', price: 50, description: 'Ideal for filming and events.', img: '/PhotoCars/volkswagen-zhuk.jpg', brand: 'Volkswagen', class: 'Compact', partner: 'Partner 1' },
+    { id: '3', title: 'Dodge Charger', price: 110, description: 'Ideal for the city and filming, driving outside the city.', img: '/PhotoCars/dodge-charger.jpg', brand: 'Dodge', class: 'Sedan', partner: 'Partner 1' },
 ];
 
 const ProductList = () => {
