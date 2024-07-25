43c7a3b6f1da6b4c22dfbb6d28144ab7d5f9d666
WeeeFamily <maaxsvt@gmail.com>
2024-07-25 08:19:33 -0800

    01234

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NavBar.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const NavBar = () => {
+    const [activeButton, setActiveButton] = useState('home');
     return (
         <div className="nav-bar">
-            <button className="nav-button" onClick={() => window.location.href='/map'}>
+            <button
+                className={`nav-button ${activeButton === 'map' ? 'active' : ''}`}
+                onClick={() => setActiveButton('map')}
+            >
                 <i className="fas fa-map-marker-alt"></i>
                 <span>Map</span>
             </button>
-            <button className="nav-button" onClick={() => window.location.href='/home'}>
+            <button
+                className={`nav-button ${activeButton === 'home' ? 'active' : ''}`}
+                onClick={() => setActiveButton('home')}
+            >
                 <i className="fas fa-home"></i>
                 <span>Home</span>
             </button>
-            <button className="nav-button" onClick={() => window.location.href='/profile'}>
+            <button
+                className={`nav-button ${activeButton === 'profile' ? 'active' : ''}`}
+                onClick={() => setActiveButton('profile')}
+            >
                 <i className="fas fa-user"></i>
                 <span>Profile</span>
             </button>
