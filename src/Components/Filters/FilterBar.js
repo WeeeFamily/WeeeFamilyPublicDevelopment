4bf46881351c92fb83a38782790e8e9251b341bd
WeeeFamily <maaxsvt@gmail.com>
2024-07-25 09:16:17 -0800

    01234

--- src/Components/Filters/FilterBar.js
+++ src/Components/Filters/FilterBar.js
@@ -3,10 +3,13 @@ import './FilterBar.css';
 
 const FilterBar = ({ onFilterChange }) => {
      const [activeFilter, setActiveFilter] = useState('all');
+
+
      const handleFilterClick = (filterName) => {
-         setActiveFilter(filterName);
-         onFilterChange(filterName);
-     };
+        setActiveFilter(filterName);
+        onFilterChange(filterName);
+         };
+
     return (
         <div className="filter-container">
             <div className={`filter ${activeFilter === 'all' ? 'active' : ''}`}>
@@ -17,32 +20,35 @@ const FilterBar = ({ onFilterChange }) => {
             </div>
             <div className={`filter ${activeFilter === 'partner' ? 'active' : ''}`}>
                 <i className="fas fa-handshake"></i>
-                <select onChange={(e) => handleFilterClick('partner', e.target.value)}>
+                <select onChange={(e) => onFilterChange('partner', e.target.value)}>
                     <option value="">Partners</option>
                     <option value="Partner 1">Jet Car</option>
                     <option value="Partner 2">Weee Cars</option>
                 </select>
+
             </div>
             <div className={`filter ${activeFilter === 'price' ? 'active' : ''}`}>
                 <i className="fas fa-dollar-sign"></i>
-                <select onChange={(e) => handleFilterClick('price', e.target.value)}>
+                <select onChange={(e) => onFilterChange('price', e.target.value)}>
                     <option value="">For price:</option>
-                    <option value="asc">Low-High</option>
-                    <option value="desc">High-Low</option>
+                    <option value="asc">Low-Hight</option>
+                    <option value="desc">Hight-Low</option>
                 </select>
+
             </div>
             <div className={`filter ${activeFilter === 'class' ? 'active' : ''}`}>
                 <i className="fas fa-car-side"></i>
-                <select onChange={(e) => handleFilterClick('class', e.target.value)}>
+                <select onChange={(e) => onFilterChange('class', e.target.value)}>
                     <option value="">Class car</option>
                     <option value="SUV">SUV</option>
                     <option value="Compact">Compact</option>
                     <option value="Sedan">Sedan</option>
                 </select>
+
             </div>
             <div className={`filter ${activeFilter === 'brand' ? 'active' : ''}`}>
                 <i className="fas fa-industry"></i>
-                <select onChange={(e) => handleFilterClick('brand', e.target.value)}>
+                <select onChange={(e) => onFilterChange('brand', e.target.value)}>
                     <option value="">Model car</option>
                     <option value="Toyota">Toyota</option>
                     <option value="Volkswagen">Volkswagen</option>
@@ -51,6 +57,6 @@ const FilterBar = ({ onFilterChange }) => {
             </div>
         </div>
     );
-     };
+};
 
-    export default FilterBar;
+export default FilterBar;
