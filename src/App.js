7abddaf4d8d73515aa2e175601e25b405bca4e75
WeeeFamily <maaxsvt@gmail.com>
2024-08-01 01:25:05 -0800

    108

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React, {useEffect} from "react";
-import {useTelegram} from "./Components/hooks/useTelegram.jsx";
+import {useTelegram} from "./Components/hooks/useTelegram";
 import {Route,Routes} from "react-router-dom";
-import Header from "./Components/header/header.jsx";
-import ProductList from "./Components/ProductList/ProductList.jsx";
-import Form from "./Components/Form/Form.jsx";
-import Profile from "./Components/Profile/Profile.jsx";
-import NavBar from "./Components/NavBar/NavBar.js";
-import Map from "./Components/Map/map.jsx";
-import AddCar from './Components/Add/addcar.jsx';
+import Header from "./Components/header/header";
+import ProductList from "./Components/ProductList/ProductList";
+import Form from "./Components/Form/Form";
+import Profile from "./Components/Profile/Profile";
+import NavBar from "./Components/NavBar/NavBar";
+import Map from "./Components/Map/map";
 
 function App() {
     const {onToggleButton,tg} =useTelegram();
@@ -19,21 +18,18 @@ function App() {
     },[])
 
   return (
-      <div className="App">
-          <Header/>
-          <Routes>
-              <Route index element={<ProductList/>}/>
-              <Route path={'form'} element={<Form/>}/>
-              <Route path={'profile'} element={<Profile/>}/>
-              <Route path={'map'} element={<Map/>}/>
-              <Route path={'addCar'} element={<AddCar/>}/>
-          </Routes>
+    <div className="App">
+        <Header/>
+        <Routes>
+            <Route index element={<ProductList/>}/>
+            <Route path={'form'} element={<Form/>}/>
+            <Route path={'profile'} element={<Profile/>}/>
+            <Route path={'map'} element={<Map/>}/>
+        </Routes>
+         <NavBar />
 
 
-          <NavBar/>
-
-
-      </div>
+    </div>
   );
 }
 
